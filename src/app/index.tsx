816e3635cc22acf2f6f0537e06f55a2edc251a1e
SASCHADDD <saschadanu23@gmail.com>
2026-05-23 00:26:42 +0700

    by pass langsung mengarahkan router ke halaman form login yang di buat praktikum

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,19 @@
-import { Link } from "expo-router";
+import { Link, Redirect } from "expo-router";
 import { Text, View, StyleSheet, TextInput, } from "react-native";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text>Halo Ganteng</Text>
+  return <Redirect href="/loginform" />;
+    // <View style={styles.container}>
+    //   <Text>Halo Ganteng</Text>
 
-      <Link href="/form" ><Text>Go to Form</Text></Link>
-      <Link href="/contoh" ><Text>Go to Contoh</Text></Link>
+    //   <Link href="/form" ><Text>Go to Form</Text></Link>
+    //   <Link href="/contoh" ><Text>Go to Contoh</Text></Link>
 
-      <TextInput 
-      placeholder="masukkan nama"
-      />
-    </View>
-  );
+    //   <TextInput 
+    //   placeholder="masukkan nama"
+    //   />
+    // </View>
+  // );
 }
 
 const styles = StyleSheet.create({
