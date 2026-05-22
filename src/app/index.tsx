cdec446268979d03242e889b6809badecba1d3fb
SASCHADDD <saschadanu23@gmail.com>
2026-05-22 21:36:19 +0700

    refactor: clean up unused imports and styles in index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,22 +1,17 @@
 import { Link } from "expo-router";
-import { Text, View, StyleSheet, TextInput, Touchable, TouchableOpacity } from "react-native";
+import { Text, View, StyleSheet, TextInput, } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Halo Ganteng</Text>
 
-      <Link href="/form" style={styles.link}><Text>Go to Form</Text></Link>
-      <Link href="/contoh" style={styles.link}><Text>Go to Contoh</Text></Link>
+      <Link href="/form" ><Text>Go to Form</Text></Link>
+      <Link href="/contoh" ><Text>Go to Contoh</Text></Link>
 
       <TextInput 
       placeholder="masukkan nama"
-      placeholderTextColor="#999"
-      style={styles.input}
       />
-      <TouchableOpacity style={styles.button} activeOpacity={0.8}>
-        <Text style </Text>
-      </TouchableOpacity>
     </View>
   );
 }
@@ -26,11 +21,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  link: {
-    backgroundColor: "#007AFF",
-    padding: 15,
-    borderRadius: 5,
-    marginTop: 20,
   },
 });
